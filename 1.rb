9dab7a9f0fa3cd39f30a14be8b17f0e0151c6ce9
Marisol Smith <marisolsmith@gmail.com>
2018-05-18 19:27:26 -0400

    empezando

---
+++ 1.rb
@@ -0,0 +1,16 @@
+class Table
+
+  def initialize(table, *collects)
+    @table = table
+    @collects = collects
+  end
+  def read
+
+  end
+  end
+end
+
+File.open('casino.txt', 'r')
+mesas = file.readlines
+puts contents
+file.close
